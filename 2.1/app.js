1646132423a3189555e9e45741a98a34d12d8acb
TorrieL <hmminicity2@163.com>
2020-01-03 01:03:49 +0800

    3.1.2-callback functions

--- 2.1/app.js
+++ 2.1/app.js
@@ -24,3 +24,27 @@ var ninja = {};//assign a new object to a variable
 var ninjaArray = [];
 ninjaArray.push({}); // adds new object to an array
 ninja.data =  {};//assign a new object as a property of another object
+
+//3.1 a simple callback example
+let text = 'Domo arigato!';
+report('Before defining functions');
+
+function useless(ninjaCallback){
+  report('In useless function')
+  return ninjaCallback();
+}
+function getText(){
+  report('In getText function')
+  return text;
+}
+report("Before making all the calls");
+assert(useless(getText) === text,"The useless function works!" +text)
+function report(tip){
+  console.log(tip)
+}
+function assert(condition,msg){
+  if(condition === true){
+    console.log(msg)
+  }
+}
+report("After the calls have been made");
