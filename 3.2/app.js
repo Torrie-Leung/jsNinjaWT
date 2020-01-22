d417a9d54e2a23fe318f0a1b20baae527104a120
TorrieL <hmminicity2@163.com>
2020-01-22 20:35:16 +0800

    sort: ascending vs descending

--- 3.2/app.js
+++ 3.2/app.js
@@ -2,4 +2,9 @@ let values = [0,5,8,1,7,4]
 values.sort((v1,v2) => {
   return v1 - v2;
 })
-console.log(values) //[0, 1, 4, 5, 7, 8]
+console.log(values) //[0, 1, 4, 5, 7, 8] ascending
+
+values.sort((v1,v2) => {
+  return v2 - v1;
+})
+console.log(values) //[8, 7, 5, 4, 1, 0] descending
