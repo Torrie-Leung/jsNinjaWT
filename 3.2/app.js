31df118014ef9f9bacbfee9caf6995fe8491e0c4
TorrieL <hmminicity2@163.com>
2020-01-23 16:31:15 +0800

    sorting a collection of unique functions

--- 3.2/app.js
+++ 3.2/app.js
@@ -8,3 +8,30 @@ values.sort((v1,v2) => {
   return v2 - v1;
 })
 console.log(values) //[8, 7, 5, 4, 1, 0] descending
+
+//sorting a collection of unique functions
+let ninja = function(){};
+ninja.name = "Scamander"
+let store = {
+  nextId: 1,
+  cache: {},
+  add: function(fn){
+    if(!fn.id){
+      fn.id = this.nextId ++
+      this.cache[fn.id] = fn;
+      console.log(this.nextId)
+      return true
+      
+    }
+  }
+};
+assert(store.add(ninja),"function was safely added.")
+assert(!store.add(ninja),"but it was only added once")
+
+
+
+function assert(condition,msg){
+  if(condition === true){
+    console.log(msg)
+  }
+}
